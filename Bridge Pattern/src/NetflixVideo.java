8400481266141086a074d2f2b7edbf18938ef2b0
Harishankar Bhat R <rhsb1997@gmail.com>
2023-07-16 20:18:27 +0530

    Bridge Pattern Implementation

---
+++ Bridge Pattern/src/NetflixVideo.java
@@ -0,0 +1,10 @@
+public class NetflixVideo extends Video {
+    public NetflixVideo(VideoProcessor videoProcessor) {
+        super(videoProcessor);
+    }
+
+    @Override
+    public void play(String videoFile) {
+        videoProcessor.process(videoFile);
+    }
+}
---
+++ Bridge Pattern/src/Video.java
@@ -0,0 +1,9 @@
+public abstract class Video {
+    protected VideoProcessor videoProcessor;
+
+    public Video(VideoProcessor videoProcessor) {
+        this.videoProcessor = videoProcessor;
+    }
+
+    public abstract void play(String videoFile);
+}
