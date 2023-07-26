a82631337b0b8c0d3b5a5bc4ac20d07dec9c088e
Harishankar Bhat R <rhsb1997@gmail.com>
2023-07-26 22:56:50 +0530

    Composite Pattern Implementation

---
+++ Composite Pattern/src/FinanceDepartment.java
@@ -0,0 +1,15 @@
+public class FinanceDepartment implements Department {
+
+    private Integer id;
+    private String name;
+
+    public FinanceDepartment(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public void printDepartmentName() {
+        System.out.println(getClass().getSimpleName());
+    }
+}
